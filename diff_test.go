204880ff22f25305e0b6f11f26e17466406a0283
daniel <daniel.wpei@hotmail.com>
2016-02-29 16:11:21 +0800

    try test

--- diff_test.go
+++ diff_test.go
@@ -1,12 +1,38 @@
 package diff
 
 import (
+	"bytes"
 	"testing"
+	"strings"
 	// . "github.com/DanielPWang/wdiff"
 )
 
-func (self Unit) GetValue(name string) float32 {
-	return 0.00
+// type Unit rune
+type Unit struct {
+	name string
+	DefaultValueName string
+	attrs map(string)float32
+	DefaultShowName  string
+	showAttr map(string) string
+}
+
+func NewUnit(str string) []Unit {
+	bytes := &[]byte(str)
+	
+}
+func (self *Unit) GetValue(name string) float32 {
+	if strings.Compare(name, "")==0 {
+		name = self.DefaultValueName
+	}
+	value, ok := self.attrs[name]
+	if !ok {
+		Log("[WARN] ", self.name, " dont have Attr ", name)
+		value = 0.00
+	}
+	return value
+}
+func (self *Unit) GetShow(name string) string {
+	return self.name;
 }
 
 func TestDiff(t *testing.T) {
